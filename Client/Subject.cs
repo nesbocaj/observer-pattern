a018e47fb4f0ca4f0e2ca51339ef689544384354
Jacob Jacobsen <jacobg.jacobsen+github@gmail.com>
2013-05-30 19:56:21 +0200

    Initial Commit

---
+++ Client/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Subject
+    {
+        private List<IObserver> _observers;
+
+        public Subject()
+        {
+            _observers = new List<IObserver>();
+        }
+
+        public void RegisterObserver(IObserver observer) { }
+
+        public void UnregisterObserver(IObserver observer) { }
+
+        public void NotifyObservers() { }
+    }
+}
